27f8c16daa6efd290e199d0ecd35bece48925caa
kgravesjohnson@gmail.com <kgravesjohnson@gmail.com>
2018-09-05 02:25:27 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,40 @@
+function add (a, b) {
+  var number = a + b;
+  return number;
+}
+
+function subtract (a,b) {
+  var number = a - b;
+  return number;
+}
+
+function multiply (a,b) {
+  var number = a * b;
+  return number;
+}
+
+function divide (a,b) { 
+  var number = a/b;
+  return number;
+}
+
+function inc(n) {
+  var number =+ n;
+  return number;
+}
+
+function dec(n) {
+  var number =- n;
+  return number;
+}
+
+function makeInt(n) { 
+  if (n === isNaN) { return NaN}
+  else {return parseInt(n,10);}
+    
+  }
+  
+  function preserveDecimal(n) {
+    if (n === isNaN) { return NaN}
+    else { return parseFloat(n);
+  }

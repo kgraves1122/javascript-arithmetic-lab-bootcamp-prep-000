0fe924a5d4b69357ee6d59d5e8461f4450ef42b8
kgravesjohnson@gmail.com <kgravesjohnson@gmail.com>
2018-09-05 00:50:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+1 + 80
+60 - 40
+2 * 3.4
+5.0 / 2.5
+
+function add (x,y) {
+  var add = x + y;
+  return add;
+}
+
+function subtract (x, y) {
+  var subtract = x - y;
+  return subtract;
+}
+
+function multiply (x,y) {
+  var multiply = x * y;
+  return multiply;
+}
+
+function divide (x,y) {
+  var divide = x / y;
+  return divide;
+}

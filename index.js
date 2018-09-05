8036270cf10e6b739925262020b1029caf135029
kgravesjohnson@gmail.com <kgravesjohnson@gmail.com>
2018-09-05 02:02:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,4 +9,21 @@ function subtract (a,b) {
   return subtract;
 }
 
-function multiply (a,b)
+function multiply (a,b) { var multiply = a * b;
+return multiply;
+}
+
+function divide (a,b) {
+  var divide = a/b;
+  return divide;
+}
+
+function inc(n) {
+  var num = parseInt(n);
+  return num;
+}
+
+function dec(n) {
+  var too = parseFloat(n);
+  return too;
+}
